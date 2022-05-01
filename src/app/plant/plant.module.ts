5eeda1029d53f486bbb54c79c5619edd5f6e3920
John Guerrero Ayala <je.guerreroa1@uniandes.edu.co>
2022-04-30 23:16:47 -0500

    feat (Plant): Add plant module

---
+++ src/app/plant/plant.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PlantComponent } from './plant.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [PlantComponent]
+})
+export class PlantModule { }
